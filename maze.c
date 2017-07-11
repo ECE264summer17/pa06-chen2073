8da74e72e7900dcbbed7a275876336774bfc937c
Hao Chen <chen2073@shay.ecn.purdue.edu>
2017-07-11 19:25:13 -0400

    pa06

--- maze.c
+++ maze.c
@@ -5,16 +5,90 @@
 #ifndef READMAZE_OFF
 Maze * readMaze(char * mazeFilename) {
 
-	//FILL IN
+int row=0, col=0;
 
+FILE * f = fopen(mazeFilename, "r");
+fseek(f, 0, SEEK_SET);
+int fptr = fgetc(f);
+
+Maze * labyrinth = (Maze *) malloc(sizeof(*labyrinth));
+
+	//count the row and column size
+	while(fptr != EOF){
+
+		if(fptr == '\n')
+			row++;
+		
+		else if(row == 0)
+			col++;
+
+	fptr = fgetc(f);}
+
+	//maze row and column size
+	labyrinth->width = col;
+	labyrinth->height = row;
+
+	//allocate space for 2-D array	
+	labyrinth->maze = (MazeSquare * * ) malloc(sizeof(MazeSquare * ) * row);
+	
+	if(labyrinth->maze == NULL)
+		return NULL;
+
+	int i;
+	for (i=0; i < row; i++){
+		(labyrinth->maze)[i] = (MazeSquare * ) malloc(sizeof(MazeSquare) * col);} 
+		
+	//assign file character to 2-D array
+	fseek(f, 0, SEEK_SET);
+
+	int j; 
+	for(i=0; i < row; i++){
+		for(j=0; j <= col; j++){
+
+			int k = fgetc(f);
+			if(k != '\n'){
+				(labyrinth->maze)[i][j].type = k;
+				(labyrinth->maze)[i][j].visited = false;}
+		}
+	}
+
+	//locate start and end position
+	fseek(f, 0, SEEK_SET);
+
+	for(i=0; i < row; i++){
+		for(j=0; j <= col; j++){
+
+			int k = fgetc(f);
+			if(k == 's'){
+				labyrinth->start.xpos = i;
+				labyrinth->start.ypos = j;}
+		
+			if(k == 'e'){
+				labyrinth->end.xpos = i;
+				labyrinth->end.ypos = j;} 
+		}
+	}
+	
+
+	//test section
+	//printf("\n");
+	//printf("height:%d, width:%d\n", labyrinth->height, labyrinth->width);
+	//printf("start(%d, %d), end(%d, %d)\n", labyrinth->start.xpos, labyrinth->start.ypos, labyrinth->end.xpos, labyrinth->end.ypos);
+
+	fclose(f);	
+	
+	return labyrinth;
 }
 #endif //#ifndef READMAZE_OFF
 
 #ifndef FREEMAZE_OFF
 void freeMaze(Maze * m) {
 	//1. Free each of the "rows" of the maze
-	
+	int i;
+	for(i=0; i < (m->height); i++){
+		free( (m->maze)[i] );}
+
 	//2. Free the "spine" of the array
-	
+	free(m->maze);
 }
 #endif //#ifndef FREEMAZE_OFF
--- solver.c
+++ solver.c
@@ -6,15 +6,132 @@
 char * solveMaze(Maze * m) {
 	char * retval = malloc(sizeof(char) * ((m->width * m->height) + 1));
 
-	MazePos mp = {.xpos = m->start.xpos, .ypos = m->start.ypos};
+	MazePos mp = {.xpos = m->start.xpos, .ypos = m->start.ypos};   //initialze start as current position
 	
-	depthFirstSolve(m, mp, retval, 0);
+	
+	
+	if( depthFirstSolve(m, mp, retval, 0) == false)
+		{retval[0] = '\0';}
+
+	else{
+		depthFirstSolve(m, mp, retval, 0);}
+
 	return retval;
 }
 
 bool depthFirstSolve(Maze * m, MazePos curpos, char * path, int step) {
+	
+	if(curpos.xpos == m->end.xpos && curpos.ypos == m->end.ypos) //check if == end position
+		{	path[step] = '\0';
+			//printf("step: %d\n" step);
+			return true;}	
 
-	//FILL IN
+	if ( ( (curpos.xpos < 0) || (curpos.xpos >= (m->height) ) ) || ((curpos.ypos >= (m->width)) || (curpos.ypos < 0) ) ) //out of bounds
+		{return false;}
 
-	return false;
-}
+	if ((m->maze)[curpos.xpos][curpos.ypos].type == '#') //reaches wall
+		{return false;}
+
+	if ((m->maze)[curpos.xpos][curpos.ypos].visited == true) //already visited
+		{return false;}
+
+			
+		(m->maze)[curpos.xpos][curpos.ypos].visited = true; //update visited
+		
+		//printf("X: %d, Y: %d\n", curpos.xpos, curpos.ypos); //REMOVE BEFORE SUBMITTING!!!!!!!
+		
+		MazePos temp = curpos;
+		temp.xpos = temp.xpos - 1;
+
+		
+		if( depthFirstSolve(m, temp, path, step+1) == true ) //check north
+			
+			{	path[step] = 'n'; //write path
+				return true;} 	//recusion		
+
+		temp = curpos;
+		temp.ypos = temp.ypos + 1;
+
+		if( depthFirstSolve(m, temp, path, step+1) == true ) //check east
+			
+			{	path[step] = 'e'; //write path
+				return true;} 	//recusion
+		
+		temp = curpos;
+		temp.xpos = temp.xpos + 1;
+
+		if( depthFirstSolve(m, temp, path, step+1) == true ) //check south
+			
+			{	path[step] = 's'; //write path
+				return true;} 	//recusion
+
+		temp = curpos;
+		temp.ypos = temp.ypos - 1;
+
+		if( depthFirstSolve(m, temp, path, step+1) == true ) //check west
+			
+			{	path[step] = 'w'; //write path
+				return true;} 	//recusion
+
+	return false;}
+	
+
+
+	/* if( ((curpos.ypos+1) < (m->width)) && ((m->maze)[curpos.xpos][curpos.ypos+1].visited == false) && ((m->maze)[curpos.xpos][curpos.ypos+1].type != '#'))	//check east of current position
+		{
+		
+
+		++curpos.ypos;	//update position
+
+		
+		(m->maze)[curpos.xpos][curpos.ypos].visited = true;	//update visited
+		
+		if( depthFirstSolve(m, curpos, path, ++step) == true )
+			{path[step] = 'E';
+			return true;} }//recusion
+		
+		else {return false;}
+
+	 if( ((curpos.xpos+1) < (m->height)) && ((m->maze)[curpos.xpos+1][curpos.ypos].visited == false) && ((m->maze)[curpos.xpos+1][curpos.ypos].type != '#') ) //check south
+		
+		{++curpos.xpos;	//update position
+		
+		
+		(m->maze)[curpos.xpos][curpos.ypos].visited = true;	//update visited
+		
+		if( depthFirstSolve(m, curpos, path, ++step) == true )
+			
+			{path[step] = 'S';
+			return true;} }//recusion
+
+		else {return false;}
+
+
+	 if( ((curpos.ypos-1) >= 0) && ((m->maze)[curpos.xpos][curpos.ypos-1].visited == false) && ((m->maze)[curpos.xpos][curpos.ypos-1].type != '#'))	//check west of current position
+		{
+		--curpos.ypos;	//update position
+		
+
+		(m->maze)[curpos.xpos][curpos.ypos].visited = true;	//update visited
+		
+		if( depthFirstSolve(m, curpos, path, ++step) == true )
+			{path[step] = 'W';
+			return true;} }//recusion
+		
+		else {return false;}
+	 
+	if( ((curpos.xpos-1) < (m->height)) && ((m->maze)[curpos.xpos-1][curpos.ypos].visited == false) && ((m->maze)[curpos.xpos-1][curpos.ypos].type != '#') ) //check north
+		
+		{++curpos.xpos;	//update position
+		
+		
+		(m->maze)[curpos.xpos][curpos.ypos].visited = true;	//update visited
+		
+		if( depthFirstSolve(m, curpos, path, ++step) == true )
+			
+			{path[step] = 'S';
+			return true;} }//recusion
+
+		else {return false;}	
+*/
+
